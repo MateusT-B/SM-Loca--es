6ebd8e348bd1cf30c99c48807d006c0ea6086e8b
mateusT <mateuscas78@gmail.com>
2024-10-24 11:40:30 -0300

    Alterações na pagina index

--- index.php
+++ index.php
@@ -20,22 +20,22 @@
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="img/fotos da page quem somos/mesa decorada 3.jpg" class="d-block w-100" alt="...">
+      <img src="img/fotos da page quem somos/mesa-decorada1.jpg" class="d-block w-100" alt="mesa decorada 1">
     </div>
     <div class="carousel-item">
-      <img src="img/fotos da page quem somos/mesa-decorada1.jpg" class="d-block w-100" alt="...">
+      <img src="img/fotos da page quem somos/mesa-decorada1.jpg" class="d-block w-100" alt="mesa decorada 2">
     </div>
     <div class="carousel-item">
-      <img src="img/fotos da page quem somos/mesa-decorada2.jpeg" class="d-block w-100" alt="...">
+      <img src="img/fotos da page quem somos/mesa-decorada3.jpg" class="d-block w-100" alt="mesa decorada 3">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
+    <span class="visually-hidden">Próximo</span>
   </button>
   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
+    <span class="visually-hidden">Anterior</span>
   </button>
 </div>
 
