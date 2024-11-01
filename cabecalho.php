e83c10e0d3a3a4a0199bdd4e70e62f824a93758d
João Pedro <jplhfernandez@gmail.com>
2024-11-01 09:37:07 -0300

    Adicionei o Icone de Pagina em todas elas

--- cabecalho.php
+++ cabecalho.php
@@ -1,4 +1,5 @@
 <!--Cabeçalho -->
+
 <header id="cabecalho" class="bg-light p-3 text-white">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
