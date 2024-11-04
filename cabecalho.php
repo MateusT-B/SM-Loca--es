c42a9d2298f2bf6f31bd2f7bece1cac066684e81
mateus <mateuscas78@gmail.com>
2024-11-04 09:07:25 -0300

    Alterações no css, cabeçalho e criação menu_cliente

--- cabecalho.php
+++ cabecalho.php
@@ -3,13 +3,10 @@
 <header id="cabecalho" class="bg-light p-3 text-white">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-        </a>
 <!-- Barra de navegação com Página Inicial etc-->
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
         <a href="index.php" class="navbar-brand " >
-                    <img id="logo" src="img/logo.png" >
+          <img id="logo" src="img/logo.png" >
         </a>
         </ul>
         <li><a href="index.php" class="nav-link px-2 text-dark">Página Inicial</a></li>
