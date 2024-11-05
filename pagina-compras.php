0b98b4bed4d3784dad6735b5ff042a22db48788d
Gabriel F <gabrielsouza191427@gmail.com>
2024-11-05 11:48:27 -0300

    adicionei o menu, o favicon

--- pagina-compras.php
+++ pagina-compras.php
@@ -4,9 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+    <link rel="shortcut icon" href="img/favicon1.png" type="image/png">
     <title>SM-Locações</title>
+    
 </head>
 <body>
+
+<!-- inclusão do cabeçalho -->
+<?php include ("cabecalho.php") ?>
+
+<div id=compras>
+    <div id=imagem_teste_compras>
+        <img   src="img/produtos/mesas/Cadeira-plastica.jpg" alt="Cadeira do Rei">
+    </div> 
+    <div>
+        <button type="button" class="btn btn-success  btn-lg" >Adicionar</button>
+    </div>
+</div>
     
 </body>
 </html>
