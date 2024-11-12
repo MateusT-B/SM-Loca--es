c9e48dc92a8202dbcf8c89179903265277957297
João Pedro <jplhfernandez@gmail.com>
2024-11-12 09:53:49 -0300

    Mudanças PHP

---
+++ admin/menu_adm_op.php
@@ -0,0 +1,27 @@
+<!--Cabeçalho -->
+<link rel="stylesheet" href="../css/bootstrap.css">
+<link rel="stylesheet" href="../css/estilo.css">
+<header id="cabecalho" class="bg-light p-3 text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+<!-- Barra de navegação com Página Inicial etc-->
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+        <a href="index.php" class="navbar-brand " >
+          <img id="logo" src="../img/logo.png" alt="mds">
+        </a>
+        </ul>
+        <li><a href="lista_produtos.php" class="nav-link px-2 text-dark">Produtos</a></li>
+          <li><a href="lista_categoria.php" class="nav-link px-2 text-dark">Categoria</a></li>
+          <li><a href="lista_usuarios.php" class="nav-link px-2 text-dark">Usuários</a></li>  
+        <!-- Botões de login e cadastro -->
+        <div class="text-end">
+          <a href="../index.php">
+            <img class="icon-img" src="../img/home.svg" alt="HOME">
+          </a>
+          <a href="logout.php">
+            <img class="icon-img" src="../img/logout.svg" alt="LOGOUT">
+          </a>
+      </div>
+      <hr>
+    </div>
+</header>
