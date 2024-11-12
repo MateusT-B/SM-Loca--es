0245fe67728218bf3f9262aeb87defb0e307a5be
ALESSANDRO RODRIGUES DE JESUS <nandok1012@gmail.com>
2024-11-12 11:45:52 -0300

    fazendo o formulario de endereço

--- cliente/pagamento.php
+++ cliente/pagamento.php
@@ -45,16 +45,7 @@
         <!-- Botão de pagamento -->
         <button type="submit" class="payment-button">Pagar Agora</button>
     </form>
-
-    <form id="">
-
-    </form>
-</div>
-
-<br>
-<?php include ("rodape.php") ?>
-
-<script>
+    <script>
      //js para aceitar apenas numero e a quantidade correta de um numero de cartao 
        document.getElementById('card-number').addEventListener('input', function (e) {
         let value = e.target.value.replace(/\D/g, ''); // Remove tudo que não for número
@@ -83,6 +74,99 @@
     });
 </script>
 
-</script>
+    <div class="form-endereco">
+    
+    <div class="row mb-3">
+      <div class="col-md-6">
+        <label for="logradouro" class="form-label">Logradouro</label>
+        <input type="text" class="form-control" id="logradouro" >
+      </div>  
+    </div>
+
+    <!-- Número -->
+    <div class="row mb-3">
+      <div class="col-md-6">
+        <label for="numero" class="form-label">Número</label>
+        <input type="number" class="form-control" id="numero" >
+      </div>  
+    </div>
+
+    <!-- Bairro -->
+    <div class="row mb-3">
+      <div class="col-md-6">
+        <label for="bairro" class="form-label">Bairro</label>
+        <input type="text" class="form-control" id="bairro" >
+      </div>  
+    </div>
+
+    <!-- Cidade -->
+    <div class="row mb-3">
+      <div class="col-md-6">
+        <label for="cidade" class="form-label">Cidade</label>
+        <input type="text" class="form-control" id="cidade" >
+      </div>  
+    </div>
+
+    <!-- Estado -->
+    <div class="row mb-3">
+      <div class="col-md-6">
+      <label for="estado" class="form-label">Estado</label>
+        <select class="form-control">
+          <option>AC</option>
+          <option>AL</option>
+          <option>AP</option>
+          <option>AM</option>
+          <option>BA</option>
+          <option>CE</option>
+          <option>DF</option>
+          <option>ES</option>
+          <option>GO</option>
+          <option>MA</option>
+          <option>MT</option>
+          <option>MS</option>
+          <option>MG</option>
+          <option>PA</option>
+          <option>PB</option>
+          <option>PR</option>
+          <option>PE</option>
+          <option>PI</option>
+          <option>RJ</option>
+          <option>RN</option>
+          <option>RS</option>
+          <option>RO</option>
+          <option>RR</option>
+          <option>SC</option>
+          <option>SP</option>
+          <option>SE</option>
+          <option>TO</option>
+        </select>
+      </div>  
+    </div>
+
+    <!-- CEP -->
+    <div class="row mb-3">
+      <div class="col-md-6">
+        <label for="cep" class="form-label">CEP</label>
+        <input type="number" class="form-control" id="cep" >
+      </div>  
+    </div>
+
+    
+    <!-- Tipo -->
+    <div class="row mb-3">
+      <div class="col-md-6">
+      <label for="tipo" class="form-label">Tipo</label>
+        <select class="form-control">
+          <option>RES</option>
+          <option>COM</option>
+        </select>
+      </div>  
+    </div>
+    </div>
+</div>
+
+<br>
+<?php include ("rodape.php") ?>
+
 </body>
 </html>
