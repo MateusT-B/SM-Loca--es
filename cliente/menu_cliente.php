8eed163b03fc4d371934f3c5a392f96f5dd5ce55
mateus <mateuscas78@gmail.com>
2024-11-14 08:39:26 -0300

    Redirecionamento para a page principal ao clicar no icone

--- cliente/menu_cliente.php
+++ cliente/menu_cliente.php
@@ -4,7 +4,7 @@
             
             <!-- Barra de navegação com Página Inicial etc -->
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <a href="../index.php" class="navbar-brand">
+                <a href="../cliente/index.php" class="navbar-brand">
                     <img id="logo" src="../img/logo.png" alt="Logo">
                 </a>
             </ul>
