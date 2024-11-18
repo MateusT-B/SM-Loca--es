e6f819fd98194978d1c7e919cc4092e318127540
João Pedro <jplhfernandez@gmail.com>
2024-11-18 10:44:51 -0300

    Realizando o começo da lista de users

--- admin/usuarios-fun/lista_usuarios-fun.php
+++ admin/usuarios-fun/lista_usuarios-fun.php
@@ -3,7 +3,7 @@
 include '../acesso_com.php';
 include '../../banco/connect.php';
 
-$lista = $conn -> query("select * from usuarios_web");
+$lista = $conn -> query("select * from usuarios_funcionarios_web");
 $row = $lista -> fetch_assoc();
 $rows = $lista -> num_rows;
 ?>
@@ -21,7 +21,7 @@ $rows = $lista -> num_rows;
     <?php include '../menu_adm_op.php'; ?>
 
     <main class="container-lista">
-        <h2 class="breadcrumb-lista alert alert-warning">Lista de Produtos</h2>
+        <h2 class="breadcrumb-lista alert alert-warning">Lista de Usuários</h2>
 
         <table class="table-lista table-hover-lista table-condensed-lista tb-opacidade-lista bg-warning-lista">
             <thead class="thead-prod">
@@ -29,10 +29,10 @@ $rows = $lista -> num_rows;
                     <th class="d-none">ID</th>
                     <th class="d-none">ID_FUNC ou ID_CLI</th>
                     <th>USUÁRIO</th>
-                    <th>SENHA</th>
                     <th>ATIVO</th>
+                    <th>TIPO</th>
                     <th>
-                        <a href="insere_produtos.php" class="btn btn-primary btn-xs">
+                        <a href="insere_usuarios-cli.php" class="btn btn-primary btn-xs">
                             <span class="add">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
@@ -46,30 +46,32 @@ $rows = $lista -> num_rows;
                 <?php do { ?>
                     <tr>
                         <td class="d-none"><?php echo $row['id']; ?></td>
-                        <?php
-                            if ($row['tipo'] == 'fun') {
-                                echo '<td class="d-none">' . $row['id_funcionario'] . '</td>';
-                            } else {
-                                echo '<td class="d-none">' . $row['id_cliente'] . '</td>';
-                            }
-                            ?>
-                        <td><?php echo $row['usuario']; ?></td>
-                        <td><?php echo $row['senha']; ?></td>
+                        <td class="d-none">
+                            <?php
+                                if ($row['tipo_func'] == 'fun') {
+                                    echo '<td class="d-none">' . $row['id_funcionario'] . '</td>';
+                                } else {
+                                    echo '<td class="d-none">' . $row['id_cliente'] . '</td>';
+                                }
+                                ?> 
+                        </td>
+                        <td class="d-none"><?php echo $row['usuario']; ?></td>
                         <td>
                             <?php
                                 if($row['ativo'] == 'Sim') {
                                     echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-                                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
-                                        </svg>';
+                                    <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
+                                    </svg>';
                                 } else {
                                     echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-ban" viewBox="0 0 16 16">
-                                            <path d="M15 8a6.97 6.97 0 0 0-1.71-4.584l-9.874 9.875A7 7 0 0 0 15 8M2.71 12.584l9.874-9.875a7 7 0 0 0-9.874 9.874ZM16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0"/>
-                                        </svg>';
+                                    <path d="M15 8a6.97 6.97 0 0 0-1.71-4.584l-9.874 9.875A7 7 0 0 0 15 8M2.71 12.584l9.874-9.875a7 7 0 0 0-9.874 9.874ZM16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0"/>
+                                    </svg>';
                                 }
-                            ?>
+                                ?>
+                        <td class="d-none"><?php echo $row['tipo']; ?></td>
                         </td>
                             <a
-                                href="update_produtos.php?id=<?php echo $row['id'] ?>" 
+                                href="update_usuarios.php?id=<?php echo $row['id'] ?>" 
                                 role="button" 
                                 class="btn btn-warning btn-block btn-xs"
                             >
